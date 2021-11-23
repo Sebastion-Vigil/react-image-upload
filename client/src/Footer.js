f16adec1e3cecb379e8a6109a6db824adb9b8d3e
Sebastion-Vigil <ServantOfChristVigil@mail.com>
2021-11-23 02:14:15 -0600

    app connected to cloudinary accnt

--- client/src/Footer.js
+++ client/src/Footer.js
@@ -5,13 +5,6 @@ import { faMediumM, faGithub  } from '@fortawesome/fontawesome-free-brands'
 export default () => (
   <footer>
     <a 
-      href='https://medium.com/p/cc96430eaece' 
-      title='Medium Article'
-      className={'small-button medium'}
-    >
-      <FontAwesomeIcon icon={faMediumM} size='3x' color='#fff' />
-    </a>
-    <a 
       href='https://github.com/Sebastion-Vigil/react-image-upload' 
       title='Github repo'
       className={'small-button github'}
